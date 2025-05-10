47e5ae920c0ee7b3888eeda5826ee7b5939616a6
Tammineni Monika <141040099+MonikaTammineni@users.noreply.github.com>
2025-05-11 03:21:09 +0530

    Add files via upload

---
+++ Fsad-project/src/test/java/com/monikatammineni/fsadproject/FsadProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monikatammineni.fsadproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsadProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
